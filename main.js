3e9601df72e0b96d9cae517f0f5b8b539547e0d2
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-12-06 21:26:47 -0300

    addStyles

--- main.js
+++ main.js
@@ -3,20 +3,23 @@
 window.addEventListener("load", function () {
     let btnContenido = document.getElementById("btnContenido");
     let contenido = document.getElementById("info");
-    // let color = '#0099FF';
     let color2 = '#FFFFFF';
     let color3 = '#445566';
     let color = "a";
-
+    
    
     contenido.style.background = '#009966';
-
+    let border = contenido.style.borderStyle = 'green 1em solid';
+    let border2 = contenido.style.borderStyle = 'yellow 0.6em solid';
     function switchColor() {
         if (color == "a") {
             contenido.style.background = color2;
+            contenido.style.border = border;
+            
             color = "b";
         } else if(color == "b"){
             contenido.style.background = color3;
+            contenido.style.border = border2;
             color = "a";
         }
     }
